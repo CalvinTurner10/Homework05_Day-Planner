25865a4110b4dd29263f3dd3fedd595ee0d43c3d
Calvin Turner <turnerjrcalvin@gmail.com>
2022-01-22 11:13:23 -0800

    working planner

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,7 @@
-
+//Shows current Date
 var currentDay = moment().format('MMM DO, YYYY');
 $('#currentDay').text(currentDay);
-
+//The store button will store it to the local storage
 $('.saveBtn').on('click', function(){
     var text = $(this).siblings('.description').val();
     var time = $(this).parent().attr('id');
@@ -9,7 +9,7 @@ $('.saveBtn').on('click', function(){
     localStorage.setItem(time, text);
 
 })
-
+// saves each item in local storage
 $("#hour7 .description").val(localStorage.getItem("hour7"));
 $("#hour8 .description").val(localStorage.getItem("hour8"));
 $("#hour9 .description").val(localStorage.getItem("hour9"));
@@ -27,4 +27,31 @@ $("#hour20 .description").val(localStorage.getItem("hour20"));
 $("#hour21 .description").val(localStorage.getItem("hour21"));
 $("#hour22 .description").val(localStorage.getItem("hour22"));
 
-fu
+//Box changes color depending on present,past, or future
+function timeTracker(){
+
+    var currentTime = moment().hour();
+    $('.time-block').each(function (){
+        var timeSlot = parseInt($(this).attr('id').split('hour')[1]);
+        console.log( timeSlot, currentTime)
+
+        if(timeSlot < currentTime){
+            $(this).addClass('past');
+            $(this).removeClass('future');
+            $(this).removeClass('present');
+        }
+
+         else if(timeSlot === currentTime){
+                $(this).addClass('present');
+                $(this).removeClass('future');
+                $(this).removeClass('past');
+         }
+                
+            else {
+                    $(this).addClass('future');
+                    $(this).removeClass('past');
+                    $(this).removeClass('present');
+        }
+    })
+}
+timeTracker();
