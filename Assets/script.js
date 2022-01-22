569941a7de801d8c1501be9e600bc8592858c706
Calvin Turner <turnerjrcalvin@gmail.com>
2022-01-22 08:15:14 -0800

    add current date

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,22 @@
 //places current date on page
 var currentDayEl = $('#currentDay')
 currentDayEl.text(moment().format("MMM Do YY"))
+
+
+var output = ''
+timeSlots.forEach((timeSlot, i) => {
+
+    var curnetHour = moment().start0f('hour').format('HH')
+    var color = ''
+    if (timeSlot.hour == currentHour){
+        color = 'present'
+    } else if (timeSlot.hour < currentHour) {
+        color = 'past'
+    } else{
+        color = 'future'
+    }
+    output+=
+    <div class ='row'>
+        <div class = 'col-lg-1 hour time-block>'
+    </div>
+})
